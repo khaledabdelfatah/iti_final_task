3cd095d4c6d7b5af6fd852c6e1a3d3ce4c3198e5
khaled Abd ElFatah Ahmed <khaledabdelfatah200@gmail.com>
2019-09-13 23:01:10 +0200

    HTMl Pages

---
+++ templates/getData.py
@@ -0,0 +1,23 @@
+# from flask import Flask, render_template, request, flash
+# import mysql.connector
+# mydb=mysql.connector.connect(
+#     host="localhost",
+#     user="root",
+#     passwd="",
+#     database="mydb"
+# )
+ 
+# # if request.method=="POST":
+# #     username=request.form("name")
+# #     print(username)
+# mycursor = mydb.cursor()
+# # if request.method=="POST":
+# #     username=request.form.get("name")
+# #     print(username)
+  
+# mycursor.execute("INSERT INTO users(name) VALUES ('{}')")
+# mydb.commit()
+# print(mycursor.rowcount, "record inserted.")
+   
+    
+        
