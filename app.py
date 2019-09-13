ddcd6e25dc3e40b416cb2cc815e6c6e9fa3ab50d
khaled Abd ElFatah Ahmed <khaledabdelfatah200@gmail.com>
2019-09-14 00:59:22 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,26 @@ mydb=mysql.connector.connect(
     host="localhost",
     user="root",
     passwd="",
-    database="mydb"
-)
+    database='mydb' 
+   )
+
 mycururso=mydb.cursor()
+#mycururso.execute("CREATE DATABASE mydb")#run this row
+# 
+# Execute This At First time
+# mycururso.execute("CREATE TABLE `customer` (`id` int(11) NOT NULL,`name` varchar(255) DEFAULT NULL,`address` varchar(255) DEFAULT NULL)")
+# mycururso.execute("INSERT INTO `customer` (`id`, `name`, `address`) VALUES(1, 'John', 'Highway 21');")
+# mycururso.execute("CREATE TABLE `product` (`product_name` varchar(15) NOT NULL,`product_id` int(5) NOT NULL,`description` varchar(90) NOT NULL,`imglink` varchar(200) NOT NULL)")
+# mycururso.execute("INSERT INTO `product` (`product_name`, `product_id`, `description`, `imglink`) VALUES('Iphone', 5, 'THis Is Iphone 11', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR88t1fMR2bpSyAzmYI-yx2t-NLrU3TuyUS_n0hvdrkVV3Cgze9'),('Macbook Pro', 6, 'This is a macbook Pro', 'https://cnet4.cbsistatic.com/img/F9suzz0c1Q7ghhQoienu2TajkIw=/868x488/2016/10/27/6cd01ecb-40cd-4615-b5f5-82993fbf9419/apple-macbook-pro-13-inch-2016-1765-026.jpg'),('MiBand', 7, 'This Is  a mi band', 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSTsJlhB3eg2QzskfNa_DLGWzkuJqVWYSN_vFb5r70sCpOkFkFo'),('galaxy s10', 9, 'This Is galaxy s10', 'https://images-na.ssl-images-amazon.com/images/I/619h1rSUcKL._SX569_.jpg'),('Cannon 4000D', 10, 'This Is Canon 4000D cam', 'https://images-na.ssl-images-amazon.com/images/I/41RV3IWWADL._SX425_.jpg'),('Wireless Keybor', 11, 'This is a Wireless Keybord', 'https://target.scene7.com/is/image/Target/GUEST_25cb8b97-a9ce-4dad-a0b7-1d7142461fc1?wid=488&hei=488&fmt=pjpeg'),('Apple Airpods', 12, 'This is Airpods', 'https://images-na.ssl-images-amazon.com/images/I/41qIPi7taiL._SX385_.jpg'),('VR Box', 13, 'Enjoy With This Amazing VR Box', 'https://rukminim1.flixcart.com/image/832/832/j5lchow0/smart-glass/u/k/4/12-nexus-original-imaevs9sg9egpjzh.jpeg?q=70');")
+# mycururso.execute("CREATE TABLE `users` (`id` int(11) NOT NULL,`name` varchar(50) NOT NULL,`email` varchar(30) NOT NULL,`address` varchar(30) NOT NULL,`role` varchar(10) DEFAULT NULL,`password` varchar(30) NOT NULL)")
+# mycururso.execute("INSERT INTO `users` (`id`, `name`, `email`, `address`, `role`, `password`) VALUES(19, 'admin', 'admin@admin.com', 'admincity', 'admin', 'admin'),(21, 'Khaled Ahmed', 'khaled.basha144@gmail.com', 'Cairo', 'user', '123456789'),(22, 'Khaled Abd ElFatah', 'khaledabdelfatah200@gmail.com', 'Mansoura', 'user', '123456789'),(23, 'user', 'user@user.com', 'User City', 'user', '123456789');")
+# mycururso.execute("ALTER TABLE `customer`ADD PRIMARY KEY (`id`);")
+# mycururso.execute("ALTER TABLE `product`ADD PRIMARY KEY (`product_id`);")
+# mycururso.execute("ALTER TABLE `users` ADD PRIMARY KEY (`id`);")
+# mycururso.execute("ALTER TABLE `customer`MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;")
+# mycururso.execute("ALTER TABLE `product`MODIFY `product_id` int(5) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;")
+# mycururso.execute("ALTER TABLE `users`MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;")
+# 
 @app.route("/")
 def home():
     
